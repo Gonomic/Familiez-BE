8fc0bd3edf5adefe832bd6b3e83f03bd9d74fdc1
GoNomics <fde@kabelfoon.nl>
2026-02-11 23:44:00 +0100

    Update PingedDbServer to use DATETIME(6) and format timestamps with millisecond precision

--- PingedDbServer.sql
+++ PingedDbServer.sql
@@ -2,22 +2,22 @@
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `PingedDbServer`$$
 CREATE PROCEDURE `PingedDbServer`(
-    IN timestampFErequest DATETIME,
-    IN timestampMWrequest DATETIME
+    IN timestampFErequest DATETIME(6),
+    IN timestampMWrequest DATETIME(6)
 )
     SQL SECURITY INVOKER
     COMMENT 'Returns ping timestamps for system health check'
 BEGIN
-    DECLARE timestampBErequest DATETIME;
-    DECLARE timestampBEanswer DATETIME;
+    DECLARE timestampBErequest DATETIME(6);
+    DECLARE timestampBEanswer DATETIME(6);
     
-    SET timestampBErequest = NOW();
-    SET timestampBEanswer = NOW();
+    SET timestampBErequest = NOW(6);
+    SET timestampBEanswer = NOW(6);
     
     SELECT 
-        timestampFErequest AS datetimeFErequest,
-        timestampMWrequest AS datetimeMWrequest,
-        timestampBErequest AS datetimeBErequest,
-        timestampBEanswer AS datetimeBEanswer;
+        CONCAT(DATE_FORMAT(timestampFErequest, '%Y-%m-%dT%H:%i:%S.'), LPAD(FLOOR(MICROSECOND(timestampFErequest) / 1000), 3, '0')) AS datetimeFErequest,
+        CONCAT(DATE_FORMAT(timestampMWrequest, '%Y-%m-%dT%H:%i:%S.'), LPAD(FLOOR(MICROSECOND(timestampMWrequest) / 1000), 3, '0')) AS datetimeMWrequest,
+        CONCAT(DATE_FORMAT(timestampBErequest, '%Y-%m-%dT%H:%i:%S.'), LPAD(FLOOR(MICROSECOND(timestampBErequest) / 1000), 3, '0')) AS datetimeBErequest,
+        CONCAT(DATE_FORMAT(timestampBEanswer, '%Y-%m-%dT%H:%i:%S.'), LPAD(FLOOR(MICROSECOND(timestampBEanswer) / 1000), 3, '0')) AS datetimeBEanswer;
 END$$
 DELIMITER ;
